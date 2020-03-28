0ee1e2ebdce83c09153e87c4cfeec9ffdf40f0b9
Anand KS <ksanand1993@gmail.com>
2020-03-28 23:51:26 +0530

    Updates

---
+++ precache-manifest.d0659d493086a22d456cab91c56612fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20de37139e8e0a0bfa84914740e724a5",
+    "url": "/Corona-Dashboard/index.html"
+  },
+  {
+    "revision": "c129557c784690eadb6b",
+    "url": "/Corona-Dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9b47b7ac1a357aefe3f5",
+    "url": "/Corona-Dashboard/static/css/main.76a68c1f.chunk.css"
+  },
+  {
+    "revision": "c129557c784690eadb6b",
+    "url": "/Corona-Dashboard/static/js/2.7c9e16f0.chunk.js"
+  },
+  {
+    "revision": "0575d16a1e494425d6a841f2e73368ff",
+    "url": "/Corona-Dashboard/static/js/2.7c9e16f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b47b7ac1a357aefe3f5",
+    "url": "/Corona-Dashboard/static/js/main.cf58155c.chunk.js"
+  },
+  {
+    "revision": "2913ea613a53786d9739",
+    "url": "/Corona-Dashboard/static/js/runtime-main.ec0a1e77.js"
+  }
+]);
